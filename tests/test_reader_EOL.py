5d63d0d62367058963a22fcfc3c9e1f23db7b702
Dominika Ferfecka <dominika.ferfecka.stud@pw.edu.pl>
2024-04-07 23:39:10 +0200

    add more complex test

--- tests/test_reader_EOL.py
+++ tests/test_reader_EOL.py
@@ -26,3 +26,29 @@ def test_EOL_r_n():
     assert(character == "a")
     character = reader.next_character()
     assert(character == EOL) 
+
+def test_EOL_three_n():
+    reader = Reader(SourceString("a\n\n\n"))
+    
+    character = reader.get_character()
+    assert(character == "a") 
+    character = reader.next_character()
+    assert(character == "\n") 
+    character = reader.next_character()
+    assert(character == "\n") 
+    character = reader.next_character()
+    assert(character == "\n") 
+
+def test_EOL_first_n():
+    reader = Reader(SourceString("\n"))
+    
+    character = reader.get_character()
+    assert(character == "\n") 
+
+def test_EOL_first_n_with_letter():
+    reader = Reader(SourceString("\na"))
+    
+    character = reader.get_character()
+    assert(character == "\n") 
+    character = reader.next_character()
+    assert(character == "a") 

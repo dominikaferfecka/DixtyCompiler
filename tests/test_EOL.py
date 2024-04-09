a50209aa13d799fae6a666fcf5393bc5eec48fcf
Dominika Ferfecka <dominika.ferfecka.stud@pw.edu.pl>
2024-04-09 18:35:30 +0200

    add more tests for EOL

--- tests/test_EOL.py
+++ tests/test_EOL.py
@@ -1,7 +1,6 @@
 from lexer import Lexer, TokenType
 from source import SourceString
 
-
 def test_n_newline():
     lexer = Lexer(SourceString("a\n a"))
     token = lexer.get_next_token()
@@ -12,6 +11,26 @@ def test_n_newline():
     assert (token.get_token_type() == TokenType.IDENTIFIER)
     assert (token.get_value() == "a")
     assert (token.get_position().get_row() == 2)
+
+
+def test_first_n_newline():
+    lexer = Lexer(SourceString("\n a"))
+    token = lexer.get_next_token()
+    assert (token.get_token_type() == TokenType.IDENTIFIER)
+    assert (token.get_value() == "a")
+    assert (token.get_position().get_row() == 2)
+
+
+def test_n_newline_two_second():
+    lexer = Lexer(SourceString("a\n\n a"))
+    token = lexer.get_next_token()
+    assert (token.get_token_type() == TokenType.IDENTIFIER)
+    assert (token.get_value() == "a")
+    assert (token.get_position().get_row() == 1)
+    token = lexer.get_next_token()
+    assert (token.get_token_type() == TokenType.IDENTIFIER)
+    assert (token.get_value() == "a")
+    assert (token.get_position().get_row() == 3)
 
 
 def test_n_newline_two():
@@ -87,8 +106,39 @@ def test_r_not_newline():
 
 
 def test_r_not_newline_two():
-    lexer = Lexer(SourceString("\r\r a"))
+    lexer = Lexer(SourceString("\r\ra"))
     token = lexer.get_next_token()
     assert (token.get_token_type() == TokenType.IDENTIFIER)
     assert (token.get_value() == "a")
     assert (token.get_position().get_row() == 1)
+    assert (token.get_position().get_column() == 3)
+
+
+def test_n_n_r_newline():
+    lexer = Lexer(SourceString("\n\na"))
+    token = lexer.get_next_token()
+    assert (token.get_token_type() == TokenType.IDENTIFIER)
+    assert (token.get_value() == "a")
+    assert (token.get_position().get_row() == 3)
+
+def test_n_n_r_newline():
+    lexer = Lexer(SourceString("\n\n\ra"))
+    token = lexer.get_next_token()
+    assert (token.get_token_type() == TokenType.IDENTIFIER)
+    assert (token.get_value() == "a")
+    assert (token.get_position().get_row() == 3)
+
+def test_n_n_r_n_newline():
+    lexer = Lexer(SourceString("\n\n\r\na"))
+    token = lexer.get_next_token()
+    assert (token.get_token_type() == TokenType.IDENTIFIER)
+    assert (token.get_value() == "a")
+    assert (token.get_position().get_row() == 4)
+
+def test_n_n_r_col_newline():
+    lexer = Lexer(SourceString("\n\n\ra"))
+    token = lexer.get_next_token()
+    assert (token.get_token_type() == TokenType.IDENTIFIER)
+    assert (token.get_value() == "a")
+    assert (token.get_position().get_row() == 3)
+    assert (token.get_position().get_column() == 2)

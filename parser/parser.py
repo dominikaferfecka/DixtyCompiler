fb9744ae15f17b53a5c8bdf05ed7c29e8a648aaf
Dominika Ferfecka <dominika.ferfecka.stud@pw.edu.pl>
2024-04-18 21:38:59 +0200

    add parsing functions

--- parser/parser.py
+++ parser/parser.py
@@ -6,7 +6,10 @@ from syntax_tree import (
     ForStatement,
     WhileStatement,
     FunStatement,
-    IfStatement
+    IfStatement,
+    OrTerm,
+    AndTerm,
+    NotTerm
 )
 
 class Parser:
@@ -59,14 +62,99 @@ class Parser:
         
         return ForStatement(identifier, expression, block, position)
 
+    # while_loop_statement ::== 'while' '(' expression ')' 'block;
+    def parse_while_statement(self):
+        if self._token.get_token_type() != TokenType.WHILE:
+            return None
+        
+        position = self._token.get_position()
+        self._token = self._lexer.get_next_token()
+
+        self.must_be(TokenType.BRACKET_OPENING, SyntaxError)
+        
+        expression = self.parse_expression()
+        if expression is None:
+            raise SyntaxError
+        
+        self.must_be(TokenType.BRACKET_CLOSING, SyntaxError)
+        
+        block = self.parse_block()
+        if block is None:
+            raise SyntaxError
+        
+        return WhileStatement(expression, block, position)
+    
+    
+    # expression ::== and_term { ‘Or’ and_term}
     def parse_expression(self):
+        left_and_term = self.parse_and_term()
+
+        if left_and_term is None:
+            return None # ?? maybe it should be error?
+        
+        while (self._token.get_token_type() == TokenType.OR):
+            position = self._token.get_position()
+            self._token = self._lexer.get_next_token()
+
+            right_and_term = self.parse_and_term()
+
+            if right_and_term is None:
+                raise SyntaxError
+            
+            left_and_term = OrTerm(left_and_term, position, right_and_term)
+        
+        return left_and_term
+    
+    # and_term ::== not_term { ‘And’ not_term};
+    def parse_and_term(self):
+        left_not_term = self.parse_not_term()
+
+        if left_not_term is None:
+            return None # ?? maybe it should be error?
+        
+        while (self._token.get_token_type() == TokenType.AND):
+            position = self._token.get_position()
+            self._token = self._lexer.get_next_token()
+
+            right_not_term = self.parse_not_term()
+
+            if right_not_term is None:
+                raise SyntaxError
+            
+            left_not_term = AndTerm(left_not_term, position, right_not_term)
+        
+        return left_not_term
+    
+
+    # not_term 	   ::== [‘Not’] comparison_term;
+    def parse_not_term(self):
+
+        position = self._token.get_position()
+
+        negation = False
+        if (self._token.get_token_type() == TokenType.NOT):
+            negation = True
+            self._token = self._lexer.get_next_token()
+        
+        comparison_term = self.parse_comparison_term()
+
+        if comparison_term is None:
+            raise SyntaxError
+        
+        if negation:
+            return NotTerm(comparison_term, position)
+    
+    # comparison_term ::== additive_term [('=='|'<'|'>'|'<='|'>=') additive_term];
+    def parse_comparison_term(self):
         pass
 
+    # block ::== '{' {statement} '}'
     def parse_block(self):
         pass
 
-    def parse_while_statement(self):
+    def parse_and_term(self):
         pass
+
 
     def parse_fun_def_statement(self):
         pass
--- parser/syntax_tree.py
+++ parser/syntax_tree.py
@@ -18,8 +18,10 @@ class ForStatement(Node):
 
 
 class WhileStatement(Node):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, expression, block, position):
+        self._expression = expression
+        self._block = block
+        self._position = position
 
 
 class FunStatement(Node):
@@ -35,3 +37,29 @@ class ReturnStatement(Node):
 class IfStatement(Node):
     def __init__(self):
         super().__init__()
+
+
+class OrTerm(Node):
+    def __init__(self, left_and_term, position, right_and_term):
+        super().__init__()
+        self._left_and_term = left_and_term
+        self._position = position
+        self._right_and_term = right_and_term
+
+
+class AndTerm(Node):
+    def __init__(self, left_not_term, position, right_not_term):
+        super().__init__()
+        self._left_not_term = left_not_term
+        self._position = position
+        self._right_not_term = right_not_term
+
+
+class NotTerm(Node):
+    def __init__(self, comparison_term, position):
+        super().__init__()
+        self._comparision_term = comparison_term
+        self._position = position
+
+
+

edeff8b555c2d7548d71581ec95b0f675866f19b
Dominika Ferfecka <dominika.ferfecka.stud@pw.edu.pl>
2024-04-21 11:20:00 +0200

    create visitor for statements

---
+++ parser/visitor.py
@@ -0,0 +1,27 @@
+from syntax_tree import Node
+from abc import ABC, abstractmethod
+
+class Visitor(ABC):
+    @abstractmethod
+    def visit_for_statement(self, for_statement):
+        pass
+
+    @abstractmethod
+    def visit_while_statement(self, while_statement):
+        pass
+
+    @abstractmethod
+    def visit_fun_def_statement(self, fun_def_statement):
+        pass
+
+    @abstractmethod
+    def visit_return_statement(self, return_statement):
+        pass
+
+    @abstractmethod
+    def visit_assign_or_call_statement(self, assign_or_call_statement):
+        pass
+
+    @abstractmethod
+    def visit_if_statement(self, if_statement):
+        pass
